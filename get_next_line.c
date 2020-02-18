9244237a59c1f9b58b3566e0924c487a6c79ff51
suhyeonahn <54574221+suhyeonahn@users.noreply.github.com>
2020-02-18 18:44:50 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -4,7 +4,9 @@
 int	fill_the_line(char *s, char **line, int data)
 {
 	int		len;
+	char	*tmp;
 
+	tmp = s;
 	len = 0;
 	if (s)
 	{
@@ -13,21 +15,15 @@ int	fill_the_line(char *s, char **line, int data)
 	}
 	if (s && s[len] == '\n')
 	{
-		if (*line != NULL)
-			*line = NULL;
-		*line = ft_substr(s, 0 ,len);
+		*line = ft_substr(s, 0, len);
 	}
 	if (!s && data == 0)
 	{
-		if (*line != NULL)
-			*line = NULL;
 		*line = ft_strdup("");
 		return (0);
 	}
 	else if (data == 0 && s[len] == '\0')
 	{
-		if (*line != NULL)
-			*line = NULL;
 		*line = ft_strdup(s);
 		return (0);
 	}
@@ -60,8 +56,7 @@ char	*keep_the_static(char *s)
 		{
 			tmp = ft_substr(s, len + 1, end - len - 1);
 			free(s);
-			s = NULL;
-			s = ft_strdup(tmp);
+			s = tmp;;
 		}
 	}
 	return (s);
@@ -69,10 +64,11 @@ char	*keep_the_static(char *s)
 
 int	get_next_line(const int fd, char **line)
 {
-	static char	*s = NULL;
+	static char	*s;
 	char	buf[BUFFER_SIZE + 1];
 	char	*tmp;
 	int		data;
+	int		result;
 
 	if (fd < 0 || line == NULL || BUFFER_SIZE == 0)
 		return (-1);
@@ -83,7 +79,6 @@ int	get_next_line(const int fd, char **line)
 			s = ft_strdup(buf);
 		else
 		{
-			tmp = NULL;
 			tmp = ft_strjoin(s, buf);
 			free(s);
 			s = tmp;
@@ -93,12 +88,13 @@ int	get_next_line(const int fd, char **line)
 	}
 	if (data < 0)
 		return (-1);
-	if (fill_the_line(s, line, data) == 0)
+	result = fill_the_line(s, line, data);
+	if (result == 0)
 	{
 		s = NULL;
 		return (0);
 	}
-	if (fill_the_line(s, line, data) == 1)
+	if (result == 1)
 		s = keep_the_static(s);
 	return (1);
 }
